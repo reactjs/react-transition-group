d06f323a7ad109ceb7bc543ae76a3638d0e71bf7
Bogdan Chadkin <trysound@yandex.ru>
2018-09-15 01:43:08 +0300

    Temporary add hacky module.exports converter

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,7 @@ module.exports = {
   ],
   plugins: [
     ['@babel/proposal-class-properties', { loose: true }],
+    'babel-plugin-add-module-exports',
     ['babel-plugin-transform-react-remove-prop-types', { mode: 'wrap' }],
   ]
 }
