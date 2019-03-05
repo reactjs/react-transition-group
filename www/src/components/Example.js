6bd73249ecdfb753696a5ca16b32e2d5dceeb5b7
Matija Marohnić <matija.marohnic@gmail.com>
2019-03-05 11:33:02 +0100

    Use iframe element directly instead of innerHTML

--- www/src/components/Example.js
+++ www/src/components/Example.js
@@ -14,16 +14,18 @@ const Example = ({ codeSandbox }) => (
     <Grid>
       <h2>Example</h2>
     </Grid>
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `
-          <iframe
-            title="${codeSandbox.title}"
-            src="https://codesandbox.io/embed/${codeSandbox.id}?fontsize=14"
-            style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
-            sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
-          />`,
+    <iframe
+      title={codeSandbox.title}
+      src={`https://codesandbox.io/embed/${codeSandbox.id}?fontsize=14`}
+      style={{
+        display: 'block',
+        width: '100%',
+        height: '500px',
+        border: 0,
+        borderRadius: 4,
+        overflow: 'hidden',
       }}
+      sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
     />
   </div>
 );
