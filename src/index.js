92b7a0fa29b4d754d53f0304a53600d1ccdfe4c9
Nikita Nafranets <eddimensi@gmail.com>
2019-04-04 01:32:53 +0300

    add export in entry point

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import CSSTransition from './CSSTransition';
 import ReplaceTransition from './ReplaceTransition';
 import TransitionGroup from './TransitionGroup';
 import Transition from './Transition';
-
+import { SwitchTransition, modes } from './SwitchTransition'
 
 module.exports = {
   Transition,
   TransitionGroup,
   ReplaceTransition,
   CSSTransition,
+  SwitchTransition,
+  switchTransitionModes: modes
 };
