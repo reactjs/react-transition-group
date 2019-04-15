a0a76d5e12595c29ff99aa6bac37a3afca30d968
Bogdan Chadkin <trysound@yandex.ru>
2019-04-15 17:20:29 +0300

    Support esm via package.json routes
    Ref #77
    
    Upgraded semantic-release-alt-publish-dir to get correct module field
    processing
    
    https://github.com/jquense/semantic-release-alt-publish-dir/commit/795af9dae9a30d06268ac64a15e5072ea8d73906

--- src/umd.js
+++
@@ -1,4 +0,0 @@
-export { default as CSSTransition } from './CSSTransition';
-export { default as ReplaceTransition } from './ReplaceTransition';
-export { default as TransitionGroup } from './TransitionGroup';
-export { default as Transition } from './Transition';
