76befdf8623570d6666593661229acbef74a0d3e
Ben Centra <ben.centra@toasttab.com>
2019-08-02 14:40:19 -0400

    fix imports to play nicely with rollup

--- src/CSSTransition.js
+++ src/CSSTransition.js
@@ -1,4 +1,4 @@
-import * as PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import addOneClass from 'dom-helpers/class/addClass';
 
 import removeOneClass from 'dom-helpers/class/removeClass';
