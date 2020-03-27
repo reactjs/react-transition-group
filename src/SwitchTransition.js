cd2866f41a7748bde71e39ff400b83a91c7d9368
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-03-28 07:10:15 +0900

    Improve JSDoc in SwitchTransition (#524)
    - Add missing periods.
    - Fix a backquote.
    - Format a code example.

--- src/SwitchTransition.js
+++ src/SwitchTransition.js
@@ -76,7 +76,7 @@ const enterRenders = {
  * Based on the selected mode and the child's key which is the `Transition` or `CSSTransition` component, the `SwitchTransition` makes a consistent transition between them.
  *
  * If the `out-in` mode is selected, the `SwitchTransition` waits until the old child leaves and then inserts a new child.
- * If the `in-out` mode is selected, the `SwitchTransition` inserts a new child first, waits for the new child to enter and then removes the old child
+ * If the `in-out` mode is selected, the `SwitchTransition` inserts a new child first, waits for the new child to enter and then removes the old child.
  *
  * **Note**: If you want the animation to happen simultaneously 
  * (that is, to have the old child removed and a new child inserted **at the same time**),
@@ -85,7 +85,6 @@ const enterRenders = {
  * instead.
  *
  * ```jsx
- *
  * function App() {
  *  const [state, setState] = useState(false);
  *  return (
@@ -94,13 +93,13 @@ const enterRenders = {
  *        key={state ? "Goodbye, world!" : "Hello, world!"}
  *        addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}
  *        classNames='fade'
- *       >
+ *      >
  *        <button onClick={() => setState(state => !state)}>
  *          {state ? "Goodbye, world!" : "Hello, world!"}
  *        </button>
  *      </CSSTransition>
  *    </SwitchTransition>
- *  )
+ *  );
  * }
  * ```
  *
@@ -200,13 +199,13 @@ SwitchTransition.propTypes = {
   /**
    * Transition modes.
    * `out-in`: Current element transitions out first, then when complete, the new element transitions in.
-   * `in-out: New element transitions in first, then when complete, the current element transitions out.`
+   * `in-out`: New element transitions in first, then when complete, the current element transitions out.
    *
    * @type {'out-in'|'in-out'}
    */
   mode: PropTypes.oneOf([modes.in, modes.out]),
   /**
-   * Any `Transition` or `CSSTransition` component
+   * Any `Transition` or `CSSTransition` component.
    */
   children: PropTypes.oneOfType([
     PropTypes.element.isRequired,
