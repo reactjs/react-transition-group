e3fb3e2112cd268d3f05b4604db98b1fb31939bc
Toru Kobayashi <koba0004@gmail.com>
2022-03-21 00:01:59 +0900

    build: setup typescript

--- .babelrc.js
+++ .babelrc.js
@@ -1,11 +1,17 @@
 module.exports = {
-  presets: [['babel-preset-jason', { runtime: false }]],
+  presets: [
+    ['babel-preset-jason', { runtime: false }],
+    '@babel/preset-typescript',
+  ],
   plugins: [
     ['babel-plugin-transform-react-remove-prop-types', { mode: 'wrap' }],
   ],
   env: {
     esm: {
-      presets: [['babel-preset-jason', { modules: false }]],
+      presets: [
+        ['babel-preset-jason', { modules: false }],
+        '@babel/preset-typescript',
+      ],
     },
   },
 };
